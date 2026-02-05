f019d90783ed83ceb291943c1c7e67e2ae4057fd
Nagarjun T K <nagarjun17204@gmail.com>
2026-02-05 20:00:32 +0530

    Day 02 operator

---
+++ Day02_Operators/Operators.py
@@ -0,0 +1,22 @@
+# Arithmetic Operators
+a = 10
+b = 5
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+# Comparison Operators
+print(a > b)
+print(a == b)
+
+# Logical Operators
+x = True
+y = False
+print(x and y)
+print(x or y)
+print(not x)
+
+# Assignment Operators
+a += 5
+print(a)
