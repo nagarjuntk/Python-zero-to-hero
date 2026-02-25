b77f5def7569c6f3ebce07c9b762aa413fd73cfb
Nagarjun T K <nagarjun17204@gmail.com>
2026-02-25 20:13:55 +0530

    Day 9: Learned Python functions including parameters and return values

---
+++ Day09_Function/Function.py
@@ -0,0 +1,39 @@
+# 🧩 Day 9 – Python Functions Practice
+
+## Q1 — Simple Function
+
+Write a function that prints:
+
+Hello Python
+
+Call the function.
+
+---
+
+## Q2 — Function with Parameter
+
+Create a function that takes a name and prints:
+
+Hello <name>
+
+---
+
+## Q3 — Add Two Numbers
+
+Create a function that takes two numbers and prints their sum.
+
+---
+
+## Q4 — Return Square ⭐
+
+Create a function that takes a number and returns its square.
+
+Print the returned value.
+
+---
+
+## Q5 ⭐⭐ Even or Odd Function (INTERVIEW FAVORITE)
+
+Create a function that takes a number and returns:
+
+Even OR Odd
